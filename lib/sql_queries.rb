20e97631182ae0544efdf09a9f07bcc03e98a754
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-13 22:57:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,9 +19,9 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount) 
-FROM users 
-LEFT JOIN pledges ON users.id=pledges.user_id 
+"SELECT users.name, SUM(pledges.amount)
+FROM users
+LEFT JOIN pledges ON users.id=pledges.user_id
 GROUP BY users.name ORDER BY pledges.amount, users.name;"
 end
 
