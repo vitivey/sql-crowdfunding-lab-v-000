9413b982777cb36c5400d1016a645b0c3f0d9f11
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-13 22:58:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,5 +37,8 @@ WHERE projects.category='music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount)
+FROM projects
+JOIN pledges ON pledges.project_id=projects.id
+GROUP BY projects.category HAVING projects.category='books';"
 end
