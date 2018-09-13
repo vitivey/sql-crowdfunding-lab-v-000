784810c091fdacc9b5f80f9d97f969cb591bb650
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-13 23:17:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,9 +25,9 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT users.name, SUM(pledges.amount)
 FROM users
-LEFT JOIN pledges ON users.id=pledges.user_id
+JOIN pledges ON users.id=pledges.user_id
 GROUP BY users.name
-ORDER BY pledges.amount, users.name;"
+ORDER BY SUM(pledges.amount), users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
