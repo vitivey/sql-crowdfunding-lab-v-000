f1984ad0450f8187c4d7decad062b9937c80f6f7
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-13 22:57:11 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT projects.category, pledges.amount
 FROM projects
 JOIN pledges ON pledges.project_id=projects.id
-WHERE projects.cateory='Music';"
+WHERE projects.category='Music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
